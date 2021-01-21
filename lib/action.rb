6f412ea572550027c40bbfb59ecebdbc7c10b3cb
neyron8(Jacko) <neyron832@gmail.com>
2021-01-21 13:50:24 +0300

    Added condition checking

--- lib/action.rb
+++ lib/action.rb
@@ -1,8 +1,5 @@
-require 'yaml'
-
 class Action
-
-  def work(condition)
+  def self.work(condition)
     if (condition['mana'] < 50) && (condition['tiredness'] < 10)
       condition['fun'] -= 5
       condition['mana'] -= 30
@@ -12,14 +9,14 @@ class Action
     condition
   end
 
-  def relax_in_nature(condition)
+  def self.relax_in_nature(condition)
     condition['fun'] += 1
     condition['mana'] -= 10
     condition['tiredness'] += 10
     condition
   end
 
-  def rest(condition)
+  def self.rest(condition)
     if condition['money'] >= 20
       condition['fun'] -= 1
       condition['mana'] += 30
@@ -30,7 +27,7 @@ class Action
     condition
   end
 
-  def go_to_bar(condition)
+  def self.go_to_bar(condition)
     if condition['money'] >= 100
       condition['fun'] += 1
       condition['mana'] += 60
@@ -41,7 +38,7 @@ class Action
     condition
   end
 
-  def drink_a_lot(condition)
+  def self.drink_a_lot(condition)
     if condition['money'] >= 150
       condition['fun'] += 5
       condition['mana'] += 90
@@ -52,7 +49,7 @@ class Action
     condition
   end
 
-  def sing_in_metro(condition)
+  def self.sing_in_metro(condition)
     condition['money'] += 50 if (condition['mana'] > 40) && (condition['mana'] < 70)
     condition['fun'] += 1
     condition['mana'] += 10
@@ -61,28 +58,45 @@ class Action
     condition
   end
 
-  def go_to_sleep(condition)
+  def self.go_to_sleep(condition)
     condition['hp'] += 90 if condition['mana'] < 30
     condition['fun'] -= 3 if condition['mana'] > 70
     condition['mana'] -= 50
     condition['tiredness'] -= 70
     condition
   end
-end
 
-
-=begin class Valera
-  attr_accessor :stats
-
-  def initialize()
-    file_name = File.expand_path("conf.yaml", __dir__)
-    @stats = YAML.safe_load(File.open(file_name))
+  def self.check_mana(mana)
+    mana.between?(0, 100)
   end
+
+  def self.check_fun(fun)
+    fun.between?(-10, 10)
+  end
+
+  def self.check_tire(tiredness)
+    tiredness.between?(0, 100)
+  end
+
+  def self.check_money(money)
+    money >= 0
+  end
+
 end
 
-valera = Valera.new
-action = Action.new
-p valera.stats['hp']
-action.rest(valera.stats)
-p valera.stats['hp']
-=end
+# p Action.check_fun(13)
+
+ class Valera
+   attr_accessor :condition
+
+   def initialize()
+     file_name = File.expand_path("conf.yaml", __dir__)
+     @condition = YAML.safe_load(File.open(file_name))
+   end
+ end
+
+ valera = Valera.new
+ p valera.condition['hp']
+ Action.rest(valera.condition)
+ p valera.condition['hp']
+ p Action.check_fun(valera.condition['fun'])
