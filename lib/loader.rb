a4a2e9996aa0ba97790781758cd3db82fc922b84
neyron8(Jacko) <neyron832@gmail.com>
2021-01-22 14:28:30 +0300

    Improved save & load

--- lib/loader.rb
+++ lib/loader.rb
@@ -3,9 +3,7 @@ require 'pathname'
 require_relative '../lib/Valera'
 
 class Loader
-  def initialize(file_name)
-    @file_name = File.expand_path("../saves/#{file_name}.yaml", __dir__)
-  end
+  @file_name
 
   def load(valera)
     valera.condition = YAML.safe_load(File.open(@file_name))
@@ -15,14 +13,21 @@ class Loader
     Pathname.new(@file_name).exist?
   end
 
-  #def load(file_name)
-  #  @file_name = File.expand_path("../saves/#{file_name}.yaml", __dir__)
-  #  data = YAML.safe_load(File.open(@file_name))
-  #  data
-  #end
+  def loader(valera)
+    loop do
+      p('Введите имя пользователя')
+      @file_name = "../saves/#{$stdin.gets.strip}.yaml"
+      break if exist
+
+      puts('Такого пользователя не существует')
+    end
+    @file_name = File.expand_path("../saves/#{@file_name}", __dir__)
+    load(valera)
+  end
 end
 
 #valera = Valera.new
 #p valera.condition
-#loader = Loader.new('testsave').load(valera)
+#loader = Loader.new.loader(valera)
 #p valera.condition
+# p valera.condition
--- lib/saver.rb
+++ lib/saver.rb
@@ -3,17 +3,22 @@ require 'pathname'
 require_relative '../lib/Valera'
 
 class Saver
-  def initialize(file_name, valera)
-    @file_name = File.expand_path("../saves/#{file_name}.yaml", __dir__)
-    @valera = valera
-  end
+  @file_name
+  @valera
 
   def save
     yaml = @valera.condition.to_yaml
     File.open(@file_name, 'w') { |file| file.write(yaml) }
   end
+
+  def saver(valera)
+    p('Введите имя пользователя')
+    @file_name = "../saves/#{$stdin.gets.strip}.yaml"
+    @valera = valera
+    save
+  end
 end
 
-# valera = Valera.new
-# valera.condition['hp'] -= 90
-# saver = Saver.new('testsave',valera).save
+#valera = Valera.new
+#valera.condition['hp'] -= 90
+#saver = Saver.new.saver(valera)
